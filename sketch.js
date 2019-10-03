74852d5559a2bb658314414815a9877ce90a0940
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-03 14:24:33 -0700

    simple drawing machine

--- sketch.js
+++ sketch.js
@@ -9,7 +9,17 @@ function setup() {
 function draw() {
 
 if (mouseIsPressed == true){
-  stroke(map(mouseX, 0, 60, 255, true));
+  stroke(map (mouseX, 0, 600, 0, 255, true));
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
+}
+
+function keyTyped(){
+
+  if (key === 's'){
+    //save this iamge
+    saveCanvas('fileName', 'png');
+
+  }
+  return false
 }
