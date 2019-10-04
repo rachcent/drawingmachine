06ca7c0d30fae4f0c34f116de984b36a58c6e40f
rachcent <35813484+rachcent@users.noreply.github.com>
2019-10-03 17:52:00 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,16 +2,24 @@ function setup() {
   createCanvas(600, 600) ;
   background(220) ;
 
+  for (var i = 0; i < 10; i++) {
+    var x = random(width);
+    var y = random(height);
+    var r = 5;
+    fill(0)
+    ellipse(x, y, r, r*2, r*2);
+  }
+
   strokeWeight(3);
 }
 
 
 function draw() {
 
+//drawing
 if (mouseIsPressed == true){
   stroke(map (mouseX, 0, 600, 0, 255, true));
   line(mouseX, mouseY, pmouseX, pmouseY);
-}
 }
 
 function keyTyped(){
@@ -22,4 +30,5 @@ function keyTyped(){
 
   }
   return false
+}
 }
